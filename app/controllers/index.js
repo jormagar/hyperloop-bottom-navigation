5fde7dc18de641e110d5eff0244367959e8ee142
Jorge Macías García <jormagar@upv.es>
2018-02-20 10:18:56 +0100

    Fixes and clean

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,15 +1,79 @@
-var Activity = require('android.app.Activity'),
+const Activity = require('android.app.Activity'),
   activity = new Activity(Ti.Android.currentActivity);
 
 const RelativeLayout = require('android.widget.RelativeLayout');
-const relativeLayout = RelativeLayout.cast($.bottomNav.findViewById(Titanium.App.Android.R.id.frame_layout));
+const relativeLayout = RelativeLayout.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.relative_layout));
+const BottomNavigationView = require('android.support.design.widget.BottomNavigationView');
+const navigation = BottomNavigationView.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.navigation));
+//const navigation = BottomNavigationView.cast($.bottomNavContainer.findViewById(resIDFromString('navigation', 'id')));
 
-relativeLayout.addView(Ti.UI.createLabel({
+function resIDFromString(variableName, resourceName) {
+  return activity.getResources().getIdentifier(variableName, resourceName, activity.getPackageName());
+}
+
+for (var i in BottomNavigationView) {
+  Ti.API.info(i);
+}
+
+navigation.setOnNavigationItemReselectedListener(new BottomNavigationView.OnNavigationItemReselectedListener({
+  onNavigationItemReselected: function (item) {
+    var message = 'Menu item selected: <' + item + '>';
+
+    Ti.API.info('On navi item reselected');
+
+    /*switch (item.getItemId()) {
+      case Titanium.App.Android.R.id.action_item1:
+        alert('Menu action item 1');
+        break;
+      case Titanium.App.Android.R.id.action_item2:
+        alert('Menu action item 2');
+        break;
+      case Titanium.App.Android.R.id.action_item3:
+        alert('Menu action item 3');
+        break;
+    }*/
+
+    $.msg.setText(message);
+
+    return false;
+  }
+}));
+
+navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener({
+  onNavigationItemSelected: function (item) {
+    var message = 'Menu item selected: <' + item.getItemId() + '>';
+
+    Ti.API.info('On navi item selected');
+
+    Ti.API.info(item.getItemId());
+
+    //navigation.getMenu().getItem(item.getItemId())
+    /*switch (item.getItemId()) {
+      case Titanium.App.Android.R.id.action_item1:
+        alert('Menu action item 1');
+        break;
+      case Titanium.App.Android.R.id.action_item2:
+        alert('Menu action item 2');
+        break;
+      case Titanium.App.Android.R.id.action_item3:
+        alert('Menu action item 3');
+        break;
+    }*/
+
+    $.msg.setText(message);
+
+    return false;
+  }
+}));
+
+$.msg = Ti.UI.createLabel({
   color: 'white',
   font: {
     fontSize: 24
   },
   text: 'I am a label'
-}));
+});
+
+relativeLayout.addView($.msg);
 
 $.index.open();
