fcab41f40b69fdb2cf2371741a9d02609ca94996
Jorge Macías García <jormagar@upv.es>
2018-02-21 10:45:17 +0100

    clean code

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,34 +4,19 @@ const Activity = require('android.app.Activity'),
 const FrameLayout = require('android.widget.FrameLayout');
 const frameLayout = FrameLayout.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.frame_layout));
 const BottomNavigationView = require('android.support.design.widget.BottomNavigationView');
+const navigation = BottomNavigationView.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.navigation));
+
+//Trying to fix first selection updating menu manually with updateMenuView method
+/*
 const BottomNavigationMenuView = require('android.support.design.internal.BottomNavigationMenuView');
 const BottomNavigationPresenter = require('android.support.design.internal.BottomNavigationPresenter');
 const BottomNavigationItemView = require('android.support.design.internal.BottomNavigationItemView');
-const navigation = BottomNavigationView.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.navigation));
 const Field = require('java.lang.reflect.Field');
-const R = require('android.R');
 
+//Trick to see exported methods inside Presenter
 for (var key in BottomNavigationPresenter){
   Ti.API.info('Key: ' + key);
 }
-/*const menuView = BottomNavigationMenuView.cast(navigation.getChildAt(0));
-const shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
-shiftingMode.setAccessible(true);
-shiftingMode.setBoolean(menuView, false);
-shiftingMode.setAccessible(false);
-
-for (let i = 0; i < menuView.getChildCount(); i++) {
-  let item = BottomNavigationItemView.cast(menuView.getChildAt(i));
-  item.setShiftingMode(false);
-  // set once again checked value, so view will be updated
-  item.setChecked(item.getItemData().isChecked());
-}*/
-
-//let currentMenuItem = navigation.getMenu().getItem(0).setChecked(true);
-
-navigation.getMenu().getItem(2).setChecked(false);
-navigation.getMenu().getItem(1).setChecked(false);
-navigation.getMenu().getItem(0).setChecked(true);
 
 const mPresenter = navigation.getClass().getDeclaredField("mPresenter");
 for (var i in mPresenter) {
@@ -45,10 +30,27 @@ for (var i in presenter) {
 }
 Ti.API.info(typeof presenter.updateMenuView);
 presenter.updateMenuView(true);
+*/
+
+//Trying to disable shiftingMode in BottomNavigationView manually
+
+/*
+const menuView = BottomNavigationMenuView.cast(navigation.getChildAt(0));
+const shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+shiftingMode.setAccessible(true);
+shiftingMode.setBoolean(menuView, false);
+shiftingMode.setAccessible(false);
+
+for (let i = 0; i < menuView.getChildCount(); i++) {
+  let item = BottomNavigationItemView.cast(menuView.getChildAt(i));
+  item.setShiftingMode(false);
+  // set once again checked value, so view will be updated
+  item.setChecked(item.getItemData().isChecked());
+}*/
 
 navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener({
   onNavigationItemSelected: function (item) {
-    let message = 'Menu item selected: ';
+    let message = 'Selected: ';
     let selected = item.getItemId();
 
     item.setChecked(true);
@@ -65,17 +67,6 @@ navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNaviga
         break;
     }
 
-    /*const menu = navigation.getMenu();
-    let size = menu.size();
-
-    for (let i = 0; i < size; i++) {
-        let menuItem = menu.getItem(i);
-        Ti.API.info('Item ' + menuItem.getItemId() + ' ' + typeof menuItem.getItemId());
-        Ti.API.info('Item selected' + selected + ' ' + typeof selected);
-        Ti.API.info(menuItem.getItemId() == selected);
-        menuItem.setChecked(menuItem.getItemId() == selected);
-    }*/
-
     $.msg.setText(message);
 
     return false;
@@ -83,11 +74,13 @@ navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNaviga
 }));
 
 $.msg = Ti.UI.createLabel({
+  left: 16,
+  right: 16,
   color: 'black',
   font: {
     fontSize: 24
   },
-  text: 'I am a label'
+  text: 'I am a label added to the FrameLayout programmatically'
 });
 
 frameLayout.addView($.msg);
