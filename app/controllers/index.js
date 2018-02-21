9226a01e2a622ab2bf7838d79a3793eb2e056e49
Jorge Macías García <jormagar@gmail.com>
2018-02-21 07:38:26 +0100

    playing with NavigationViewPresenter

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -4,18 +4,54 @@ const Activity = require('android.app.Activity'),
 const FrameLayout = require('android.widget.FrameLayout');
 const frameLayout = FrameLayout.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.frame_layout));
 const BottomNavigationView = require('android.support.design.widget.BottomNavigationView');
+const BottomNavigationMenuView = require('android.support.design.internal.BottomNavigationMenuView');
+const BottomNavigationPresenter = require('android.support.design.internal.BottomNavigationPresenter');
+const BottomNavigationItemView = require('android.support.design.internal.BottomNavigationItemView');
 const navigation = BottomNavigationView.cast($.bottomNavContainer.findViewById(Titanium.App.Android.R.id.navigation));
+const Field = require('java.lang.reflect.Field');
 const R = require('android.R');
 
-for (var i in BottomNavigationView) {
+for (var key in BottomNavigationPresenter){
+  Ti.API.info('Key: ' + key);
+}
+/*const menuView = BottomNavigationMenuView.cast(navigation.getChildAt(0));
+const shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+shiftingMode.setAccessible(true);
+shiftingMode.setBoolean(menuView, false);
+shiftingMode.setAccessible(false);
+
+for (let i = 0; i < menuView.getChildCount(); i++) {
+  let item = BottomNavigationItemView.cast(menuView.getChildAt(i));
+  item.setShiftingMode(false);
+  // set once again checked value, so view will be updated
+  item.setChecked(item.getItemData().isChecked());
+}*/
+
+//let currentMenuItem = navigation.getMenu().getItem(0).setChecked(true);
+
+navigation.getMenu().getItem(2).setChecked(false);
+navigation.getMenu().getItem(1).setChecked(false);
+navigation.getMenu().getItem(0).setChecked(true);
+
+const mPresenter = navigation.getClass().getDeclaredField("mPresenter");
+for (var i in mPresenter) {
   Ti.API.info(i);
 }
+mPresenter.setAccessible(true);
+
+const presenter = BottomNavigationPresenter.cast(mPresenter.get(navigation));
+for (var i in presenter) {
+  Ti.API.info('Presenter: ' + i);
+}
+Ti.API.info(typeof presenter.updateMenuView);
+presenter.updateMenuView(true);
 
 navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener({
   onNavigationItemSelected: function (item) {
     let message = 'Menu item selected: ';
-    let result = false;
     let selected = item.getItemId();
+
+    item.setChecked(true);
 
     switch (selected) {
       case Titanium.App.Android.R.id.action_item1:
@@ -42,7 +78,7 @@ navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNaviga
 
     $.msg.setText(message);
 
-    return result;
+    return false;
   }
 }));
 
